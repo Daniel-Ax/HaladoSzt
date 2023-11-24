baa724fd6ca15c5c7d425b57c03d282d241cd66b
Daniel-Ax <finywow@gmail.com>
2023-11-24 15:48:46 +0100

    product module

--- TextbookMarketplace/TextbookMarketplaceApp/models.py
+++ TextbookMarketplace/TextbookMarketplaceApp/models.py
@@ -8,3 +8,12 @@ class YourModel(models.Model):
     def __str__(self):
         return self.title
 
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+
