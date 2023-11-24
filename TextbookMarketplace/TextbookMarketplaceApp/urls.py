f0782fb7656765b046d0f4b8da74825aa32f7cfd
Daniel-Ax <finywow@gmail.com>
2023-11-24 18:01:46 +0100

    added stuff

--- TextbookMarketplace/TextbookMarketplaceApp/urls.py
+++ TextbookMarketplace/TextbookMarketplaceApp/urls.py
@@ -1,8 +1,12 @@
 # WebshopApp/urls.py
 
 from django.urls import path
-from .views import index
+from .views import index, checkout, sell_notes, login, reg
 
 urlpatterns = [
     path('', index, name='index'),
+    path('checkout/', checkout, name='checkout'),  # Új URL a fizető oldalhoz
+    path('sell_notes/', sell_notes, name='sell_notes'), #Új jegyzet hozzáadása
+    path('login', login, name='login'), #login view path
+    path('reg/', reg, name='reg') #reg view path
 ]
--- TextbookMarketplace/TextbookMarketplaceApp/views.py
+++ TextbookMarketplace/TextbookMarketplaceApp/views.py
@@ -1,6 +1,30 @@
 # WebshopApp/views.py
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     return render(request, 'index.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def sell_notes(request):
+    return render(request, 'sell_notes.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def reg(request):
+    return render(request, 'reg.html')
+
+def registration_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Felhasználó sikeresen regisztrálva, átirányítás az új nézetbe vagy oldalra.
+            return redirect('login')  # Cseréld le az 'index' azonosítót az általad kívántra.
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration.html', {'form': form})
