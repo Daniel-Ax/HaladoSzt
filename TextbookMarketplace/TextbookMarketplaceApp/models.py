fd65d14db775301c96cae4a5c2ec944e7b30f51f
Kristof Halmai <25053497+halmaikristof@users.noreply.github.com>
2023-12-10 17:39:39 +0100

    saving images with unique filenames

--- TextbookMarketplace/TextbookMarketplaceApp/models.py
+++ TextbookMarketplace/TextbookMarketplaceApp/models.py
@@ -1,3 +1,6 @@
+import os
+import uuid
+
 from PIL import Image
 from django.contrib.auth.models import User
 from django.db import models
@@ -21,13 +24,18 @@ class Product(models.Model):
     is_available = models.BooleanField(default=True)
 
     def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
         if self.image:
+            ext = os.path.splitext(self.image.name)[1]
+            unique_filename = f'{uuid.uuid4()}{ext}'
+            self.image.name = unique_filename
+            super().save(*args, **kwargs)
             img = Image.open(self.image.path)
             if img.height > 300 or img.width > 300:
                 output_size = (300, 300)
                 img.thumbnail(output_size)
                 img.save(self.image.path)
+        else:
+            super().save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse('product_detail', args=[str(self.id)])
