3dfd2d6cb3cffd09dc77bb9648996dc7e97ce598
AiliT <14350286+AiliT@users.noreply.github.com>
2023-12-15 17:07:20 +0100

    Removed a print statement and added searching to My Products

--- TextbookMarketplace/TextbookMarketplaceApp/views.py
+++ TextbookMarketplace/TextbookMarketplaceApp/views.py
@@ -29,7 +29,6 @@ class IndexView(TemplateView):
     template_name = 'index.html'
 
     def post(self, request, *args, **kwargs):
-        print(request.POST)
         context = self.get_context_data()
         if request.POST['searchtext']:
             searchtext = request.POST['searchtext']
@@ -46,6 +45,14 @@ class IndexView(TemplateView):
 
 class MyProductsView(LoginRequiredMixin, TemplateView):
     template_name = 'my_products.html'
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data()
+        if request.POST['searchtext']:
+            searchtext = request.POST['searchtext']
+            context['products'] = [o for o in context['products'] if o.contains_text(searchtext)]
+            context['searchtext'] = searchtext
+        return render(request, self.template_name, context)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
